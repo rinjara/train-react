e719b638818114d14f8bf769643c369c18f9f7ef
Krystyna Sharhorodska <rinjara@gmail.com>
2022-10-02 17:22:20 +0300

    Img fetch setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,26 @@
-export const App = () => {
-  return (
+import { Component } from "react"
+
+export class App extends Component {
+  state = {
+  imgs: [],
+  }
+  getImages = () => {
+    fetch("https://dog.ceo/api/breeds/image/random/5").then(
+      (res) => {
+        return res.json()
+      }
+    ).then(
+      (res) => {
+        console.log(res);
+        this.setState({ imgs: res.message })
+      }
+    )
+  };
+
+  render() {
+    const { imgs } = this.state;
+    const { getImages } = this;
+    return (
     <div
       style={{
         height: '100vh',
@@ -10,7 +31,13 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+        <button type="button" onClick={getImages}>Find pictures</button>
+        <ul>
+          {imgs.map(
+            item => <li key={item}><img src={item} alt={item} width="300" /></li>
+          )}
+        </ul>
     </div>
   );
+}
 };
